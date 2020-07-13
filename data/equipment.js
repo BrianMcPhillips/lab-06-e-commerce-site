88cee7f4711b072d32b6c4f27c6e09379d3fc007
Brian McPhillips <B.McPhillips89@gmail.com>
2020-07-13 15:53:07 -0700

    JavaScript completed, still need to format CSS and write tests.

---
+++ data/equipment.js
@@ -0,0 +1,88 @@
+const monitors = {
+    id: 'monitors',
+    name: 'Genelec 8010',
+    image: 'Genelec 8010.jpeg',
+    description: 'A great pair of starter monitors for a small space.',
+    category: 'speakers',
+    price: 350.00,
+    cost: 175.00
+
+};
+
+const audioInterface = {
+    id: 'audio-interface',
+    name: 'Solid State Logic SSL2+',
+    image: 'SSL SSL2+.jpeg',
+    description: 'Awesome beginner level Audio Interface from a Legendary Company',
+    category: 'interfaces',
+    price: 280.00,
+    cost: 140.00
+
+};
+
+const microphone = {
+    id: 'starter-microphone',
+    name: 'Neumann TLM 102',
+    image: 'Neumann TLM 102.jpeg',
+    description: 'Awesome Starter Microphone',
+    category: 'microphones',
+    price: 699.00,
+    cost: 350.00
+
+};
+
+const headphones = {
+    id: 'headphones',
+    name: 'Audio-Technica ATH-M50x',
+    image: 'M50x.jpeg',
+    description: 'A great pair of starter headphones for recording with a flat frequency response.',
+    category: 'head-phones',
+    price: 150.00,
+    cost: 75.00
+
+};
+
+const midiController = {
+    id: 'midi-controller',
+    name: 'Native Instruments Komplete Kontrol S49 mk2',
+    image: 'S49.jpeg',
+    description: 'Awesome Midi Controller for NI Komplete',
+    category: 'keyboards',
+    price: 670.00,
+    cost: 335.00
+
+};
+
+const hardwareSynth = {
+    id: 'synthesizer',
+    name: 'Roland SE-02',
+    image: 'SE-02.jpeg',
+    description: 'Great Analog hardware synth',
+    category: 'sound-modules',
+    price: 350.00,
+    cost: 175.00
+
+};
+
+const channelStrip = {
+    id: 'channel-strip',
+    name: 'Vintech X73i',
+    image: 'X73i.jpeg',
+    description: 'Amazing clone of the legendary Neve 1073',
+    category: 'outboard-gear',
+    price: 1375.00,
+    cost: 650.00
+
+};
+
+const equipment = [
+    monitors,
+    audioInterface,
+    microphone,
+    headphones,
+    midiController,
+    hardwareSynth,
+    channelStrip
+];
+
+export default equipment;
