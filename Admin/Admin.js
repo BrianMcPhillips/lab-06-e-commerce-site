741cb1e67c64daaf78844eb19e9a4ada2f21ecf2
Brian McPhillips <B.McPhillips89@gmail.com>
2020-07-16 22:07:57 -0700

    getting things to set in local storage, not having any luck with an error message with toFixed.

--- Admin/Admin.js
+++ Admin/Admin.js
@@ -1,3 +1,4 @@
+import { getEquipment } from '../common/Utils.js';
 const myForm = document.getElementById('form');
 
 myForm.addEventListener('submit', (event) => {
@@ -5,4 +6,21 @@ myForm.addEventListener('submit', (event) => {
     event.preventDefault();
     
     const formData = new FormData(myForm);
+    const newProduct = {
+        id: formData.get('id'),
+        name: formData.get('name'),
+        description: formData.get('description'),
+        image: formData.get('image'),
+        price: formData.get('price'),
+
+    };
+    const existingProduct = getEquipment();
+
+    existingProduct.push(newProduct);
+
+    const stringyEquipment = JSON.stringify(existingProduct);
+
+    localStorage.setItem('EQUIPMENT', stringyEquipment);
+
+    window.location = '../Products/index.html';
 });
